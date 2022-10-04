c0703a5a6fd6ac6da83a86bfc8b5644313d19952
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2022-10-03 21:06:08 -0400

    Create diameter_of_binary_tree.py

---
+++ trees/diameter_of_binary_tree.py
@@ -0,0 +1,24 @@
+class Solution(object):
+  # Time: O(n), Space: O(n)
+    def diameterOfBinaryTree(self, root):
+        diameter = 0
+
+        def longest_path(node):
+            if not node:
+                return 0
+            nonlocal diameter
+            # recursively find the longest path in
+            # both left child and right child
+            left_path = longest_path(node.left)
+            right_path = longest_path(node.right)
+
+            # update the diameter if left_path plus right_path is larger
+            diameter = max(diameter, left_path + right_path)
+
+            # return the longest one between left_path and right_path;
+            # remember to add 1 for the path connecting the node and its parent
+            return max(left_path, right_path) + 1
+
+        longest_path(root)
+        return diameter
+# Use DFS
