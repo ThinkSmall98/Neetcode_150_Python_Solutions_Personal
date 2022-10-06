97d68d19a69cd157c476673ed3f396f96a9f7f0d
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2022-10-05 23:10:16 -0400

    Create lowest_common_ancestor_of_a_binary_search_tree.py

---
+++ trees/lowest_common_ancestor_of_a_binary_search_tree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # Time: O(N) where N = num of nodes in BST, Space: O(1)
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        p_val, q_val = p.val, q.val
+        node = root
+        while node:
+            parent_val = node.val
+            if p_val > parent_val and q_val > parent_val:
+                node = node.right
+            elif p_val < parent_val and q_val < parent_val:
+                node = node.left
+            else:
+                return node
+        
