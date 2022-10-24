a4ffb5444fe8e7115a37c3e40c8237b388f75456
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2022-10-23 22:26:23 -0400

    Create kth_largest_element_in_stream.py

---
+++ heap/kth_largest_element_in_stream.py
@@ -0,0 +1,23 @@
+# Time: O(N*log(N) + M*log(k)) where N = len of nums & M = # of calls to add() 
+
+from heapq import heapify, heappop, heappush
+class KthLargest:
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.heap = nums
+        heapq.heapify(self.heap)
+        
+        while len(self.heap) > k: # Get rid of smallest values if len(heap) > k
+            heapq.heappop(self.heap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.heap, val)
+        if len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+        return self.heap[0]
+        
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
