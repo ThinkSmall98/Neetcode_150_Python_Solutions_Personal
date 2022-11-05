9706ca91afe471d52d6f7757298d33b2ff50ac64
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2022-11-04 22:09:42 -0400

    Create combination_sum_2.py

---
+++ backtracking/combination_sum_2.py
@@ -0,0 +1,20 @@
+# Time: O(2^n), Space: O(n)
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        res = []
+
+        def backtrack(comb, remain, pos):
+            if remain == 0:
+                res.append(comb[:]) # return deep copy
+                return
+            for i in range(pos, len(candidates)):
+                if i > pos and candidates[i] == candidates[i - 1]:
+                    continue
+                if remain < candidates[i]: # optimize by stopping when exceeding remaining val
+                    break
+                comb.append(candidates[i])
+                backtrack(comb, remain - candidates[i], i + 1)
+                comb.pop()
+        backtrack([], target, 0)
+        return res
