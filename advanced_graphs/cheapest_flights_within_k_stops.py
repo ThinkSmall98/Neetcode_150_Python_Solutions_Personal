9bb90eb8dd5fbbbac15210ce17ad931c6cdee5f3
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2022-11-20 23:12:57 -0500

    Create cheapest_flights_within_k_stops.py

---
+++ advanced_graphs/cheapest_flights_within_k_stops.py
@@ -0,0 +1,16 @@
+# Time: O(E*k), more generally O(E*V) for Bellman-Ford
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        prices = [float('inf')] * n
+        prices[src] = 0
+
+        for i in range(k + 1):
+            tmpPrices = prices.copy()
+
+            for s, d, p in flights: # source, dest, price
+                if prices[s] == float('inf'): # can't reach here
+                    continue
+                if prices[s] + p < tmpPrices[d]:
+                    tmpPrices[d] = prices[s] + p
+            prices = tmpPrices
+        return prices[dst] if prices[dst] != float('inf') else -1
