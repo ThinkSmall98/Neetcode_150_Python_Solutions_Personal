3ec550a8450d3f3b41e30c87a0e3d72e60bd7376
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2022-11-23 14:08:03 -0500

    Update climbing_stairs.py

--- 1D_DP/climbing_stairs.py
+++ 1D_DP/climbing_stairs.py
@@ -5,10 +5,7 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         one, two = 1, 1
         for i in range(n - 1):
-            temp = two
-            two += one
-            one = temp
-
+            two, one = two + one, two
         return two
 
 # Fibonnaci numbers
