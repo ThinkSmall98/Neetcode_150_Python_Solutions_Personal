6a46086c854ff934244e6a5462c5d9eccf80e1c6
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2022-11-24 22:47:59 -0500

    Create longest_palindromic_substring.py

---
+++ 1D_DP/longest_palindromic_substring.py
@@ -0,0 +1,28 @@
+# Time: O(n^2)
+# Space: O(1)
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        res = ""
+        resLen = 0
+
+        for i in range(len(s)):
+            if resLen > 0 and len(s) - 1 - i < resLen // 2:
+                break
+            # odd
+            l, r = i, i
+            while l >= 0 and r < len(s) and s[r] == s[l]:
+                if r + 1 - l > resLen:
+                    res = s[l: r + 1]
+                    resLen = r + 1 - l
+                l -= 1
+                r += 1
+            # even
+            l, r = i, i + 1
+            while l >= 0 and r < len(s) and s[r] == s[l]:
+                if r + 1 - l > resLen:
+                    res = s[l: r + 1]
+                    resLen = r + 1 - l
+                l -= 1
+                r += 1
+
+        return res
