dd37b4b2d7b40b12fa2d04e24d2006c632b5011c
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2022-11-27 11:33:42 -0500

    Create decode_ways.py

---
+++ 1D_DP/decode_ways.py
@@ -0,0 +1,18 @@
+# Time: O(n)
+# Space: O(1)
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if s[0] == '0':
+            return 0
+
+        two_back, one_back = 1, 1
+        for i in range(1,len(s)):
+            curr = 0
+            if s[i] != '0':
+                curr = one_back
+            two_digit = int(s[i - 1: i + 1])
+            if two_digit >= 10 and two_digit < 27:
+                curr += two_back
+            two_back = one_back
+            one_back = curr
+        return one_back
