ea0eef408135a81eeee40e043e215495a880e5f0
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2022-11-26 21:21:38 -0500

    Update longest_palindromic_substring.py

--- 1D_DP/longest_palindromic_substring.py
+++ 1D_DP/longest_palindromic_substring.py
@@ -10,19 +10,17 @@ class Solution:
                 break
             # odd
             l, r = i, i
-            while l >= 0 and r < len(s) and s[r] == s[l]:
-                if r + 1 - l > resLen:
-                    res = s[l: r + 1]
-                    resLen = r + 1 - l
-                l -= 1
-                r += 1
+            res, resLen = self.isPali(s, l, r, resLen, res)
             # even
             l, r = i, i + 1
-            while l >= 0 and r < len(s) and s[r] == s[l]:
-                if r + 1 - l > resLen:
-                    res = s[l: r + 1]
-                    resLen = r + 1 - l
-                l -= 1
-                r += 1
-
+            res, resLen = self.isPali(s, l, r, resLen, res)
         return res
+        
+    def isPali(self, s, l, r, resLen = 0, res = ""):
+        while l >= 0 and r < len(s) and s[r] == s[l]:
+            if r + 1 - l > resLen:
+                res = s[l: r + 1]
+                resLen = r + 1 - l
+            l -= 1
+            r += 1
+        return res, resLen
