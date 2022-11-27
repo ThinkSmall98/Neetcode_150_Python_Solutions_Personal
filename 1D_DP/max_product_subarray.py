426562790190159c4b73a34d685320e58eb3dc56
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2022-11-27 12:20:05 -0500

    Create max_product_subarray.py

---
+++ 1D_DP/max_product_subarray.py
@@ -0,0 +1,11 @@
+# Time: O(n)
+# Space: O(1)
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        res = nums[0]
+        curMax, curMin = 1, 1
+
+        for n in nums:
+            curMax, curMin = max(n * curMax, n * curMin, n), min(n * curMax, n * curMin, n)
+            res = max(res, curMax)
+        return res
