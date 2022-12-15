b9b992ad1c4316f95a495369d97ef75b6fb19104
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2022-12-15 11:04:10 -0500

    Create insert_interval.py

---
+++ Intervals/insert_interval.py
@@ -0,0 +1,16 @@
+# Time: O(n) where n = len(intervals)
+# Space: O(n) bc of res arr
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
+        for interval in intervals:
+            if newInterval[1] < interval[0]:
+                res.append(newInterval)
+                newInterval = interval
+            elif newInterval[0] > interval[1]:
+                res.append(interval)
+            else:
+                newInterval = [min(newInterval[0], interval[0]), max(newInterval[1], interval[1])]
+        res.append(newInterval)
+        return res
