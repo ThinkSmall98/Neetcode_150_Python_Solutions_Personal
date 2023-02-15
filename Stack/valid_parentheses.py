7183ead194f9a79a7ebbed130f9cc7ada0f08583
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2023-02-14 22:46:59 -0500

    Update valid_parentheses.py

--- Stack/valid_parentheses.py
+++ Stack/valid_parentheses.py
@@ -7,10 +7,11 @@ class Solution(object):
             if char in map_char:
                 if stack:
                     top_element = stack.pop() 
+                    if map_char[char] != top_element:
+                        return False
                 else:
-                    top_element = "0" # dummy value
-                if map_char[char] != top_element:
-                    return False
+                    return False # found closed bracket before any open ones
+                
             # if char is opening bracket
             else:
                 stack.append(char)
@@ -18,4 +19,9 @@ class Solution(object):
         return not stack
 '''
 Open/closed Parantheses must be next to each other
+1. Start with dict mapping closed to open parentheses
+2. Create stack to keep track of open parentheses
+3. Go thru each char, find last element by popping if stack not empty else return False immediately
+4. if char not in dict keys, append to stack bc it's open bracket
+5. return True if stack empty else False
 '''
