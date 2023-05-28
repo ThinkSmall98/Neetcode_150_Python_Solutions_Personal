c661c178e22341b9de6f4ee20898a963aabe5b03
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2023-05-27 23:08:13 -0400

    Update valid_palindrome.py

--- two_pointers/valid_palindrome.py
+++ two_pointers/valid_palindrome.py
@@ -15,3 +15,10 @@ class Solution:
             left += 1
             right -= 1
         return True
+    
+    # Time: O(n)
+    # Space: O(n)
+    def isPalindrome(self, s: str) -> bool:
+        s = ''.join(char for char in s if char.isalnum())
+        s = s.lower()
+        return s == s[::-1]
