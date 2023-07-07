e565d6190887f20a7cd34e54a9c5395ecc06a117
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2023-07-06 23:03:57 -0400

    Update longest_repeating_char_replacement.py

--- sliding_window/longest_repeating_char_replacement.py
+++ sliding_window/longest_repeating_char_replacement.py
@@ -3,8 +3,8 @@ class Solution:
     # Space: O(n)
     def characterReplacement(self, s: str, k: int) -> int:
         count = {}
-        res = 0
-        left = 0 
+        left = 0
+        # res = 0
         max_freq = 0
         for right in range(len(s)):
             count[s[right]] = 1 + count.get(s[right], 0)
@@ -13,6 +13,9 @@ class Solution:
             if (right - left + 1) - max_freq > k:
                 count[s[left]] -= 1
                 left += 1
-            res = max(res,right - left + 1)
-        return res
-# Keep track of when new letters appear & try to go those?
+            # else:
+            #     res = max(res, right - left + 1)
+        return (right - left + 1)
+'''
+Don't actually need res bc the sliding window will always adjust to be the max len of longest substring
+'''
