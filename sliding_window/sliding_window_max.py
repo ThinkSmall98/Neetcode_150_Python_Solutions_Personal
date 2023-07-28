d82173347058f315c3e608d54dbdce0d68c00f6a
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2023-07-27 23:16:05 -0400

    Update sliding_window_max.py

--- sliding_window/sliding_window_max.py
+++ sliding_window/sliding_window_max.py
@@ -25,3 +25,20 @@ class Solution:
         return output
 
 # Brute force: iterate over all windows & then iterate thru all elements in a window
+# Approach 2: Remove left pointer
+    # Time: O(n)
+    # Space: O(n)
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        q = deque() # indices
+        res = []
+        left = 0
+
+        for right, val in enumerate(nums):
+            while q and nums[q[-1]] <= val:
+                q.pop()
+            q.append(right)
+            if q[0] == right - k: # remove left most value if it's outside window
+                q.popleft()
+            if right >= k - 1:
+                res.append(nums[q[0]])
+        return res
