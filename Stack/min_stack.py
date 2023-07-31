7aa7cd5dfe5e4cedec1db481b097ac83e20965a7
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2023-07-30 22:43:11 -0400

    Create min_stack.py

---
+++ Stack/min_stack.py
@@ -0,0 +1,24 @@
+class MinStack:
+    # Time: O(1)
+    # Space: O(n)
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+    def getMin(self) -> int:
+        return self.minStack[-1]
+
+# don't forget to put self in front of each one
+# have another array keeping track of min value at each index
