183a099cdb54300910964626b11caa9a0f57df84
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2023-08-12 11:34:55 -0400

    Create binary_search.py

---
+++ binary_search.py
@@ -0,0 +1,14 @@
+class Solution:
+    # Time: O(log(n))
+    # Space: O(1)
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + ((right - left) // 2)
+            if target == nums[mid]:
+                return mid
+            elif target < nums[mid]:
+                right = mid - 1
+            else: # target > nums[mid]
+                left = mid + 1
+        return -1
