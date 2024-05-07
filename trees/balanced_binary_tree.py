9fd38e7213432c726a0969e3252eb57b03d37739
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2024-05-07 15:47:20 -0400

    Update balanced_binary_tree.py

--- trees/balanced_binary_tree.py
+++ trees/balanced_binary_tree.py
@@ -5,23 +5,17 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    # Time: O(n), Space: O(n)
-    def isBalancedHelper(self, root: Optional[TreeNode]) -> bool:
-        if not root:
-            return True, -1
-        # check subtrees to see if they're balanced
-        leftIsBalanced, leftHeight = self.isBalancedHelper(root.left)
-        if not leftIsBalanced:
-            return False, leftHeight
-        rightIsBalanced, rightHeight = self.isBalancedHelper(root.right)
-        if not rightIsBalanced:
-            return False, rightHeight
-        # if substrees balanced, check if current tree is balanced using height
-        return (abs(leftHeight - rightHeight) <= 1), 1 + max(leftHeight, rightHeight)
-        
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        return self.isBalancedHelper(root)[0]
-        
-# Bottom-up recursion
-'''
-bottom-up approach is a reverse of the logic of the top-down approach since we first check if the child subtrees are balanced before comparing their heights
+        # sub-function to find if balanced & height
+        def dfs(root): # return bool, height of tree
+            if not root:
+                return (True, 0)
+            left_balanced, left_h = dfs(root.left)
+            right_balanced, right_h = dfs(root.right)
+            current_balanced = (left_balanced and right_balanced and 
+                            abs(left_h - right_h) <= 1)
+            if not current_balanced:
+                return (False, 0)
+            return (True, 1 + max(left_h, right_h))
+        return dfs(root)[0]
+
