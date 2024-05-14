d8cda56638228572243fa8ef21278fb608938104
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2024-05-13 23:13:34 -0400

    Create construct_binary_tree_from_preorder&inorder_traversal.py

---
+++ trees/construct_binary_tree_from_preorder&inorder_traversal.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not preorder or not inorder:
+            return None
+        root = TreeNode(preorder[0])
+        root_i = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1 : root_i + 1], inorder[:root_i]) 
+        # don't count root_i in inorder list
+        root.right = self.buildTree(preorder[root_i + 1:], inorder[root_i + 1 : ]) 
+        return root
