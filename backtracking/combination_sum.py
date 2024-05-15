399b40b5d90c1c3145898a582bf7eca808c5cf9f
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2024-05-15 15:15:45 -0400

    Update combination_sum.py

--- backtracking/combination_sum.py
+++ backtracking/combination_sum.py
@@ -4,16 +4,19 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
-        def dfs(i, cur, total):
-            if total == target:
-                res.append(cur.copy())
+        def dfs(i, arr, curr_sum):
+            if curr_sum == target:
+                # append shallow copy of arr bc of recursion
+                res.append(arr.copy())
+                return 
+            if curr_sum > target or i == len(candidates):
                 return
-            if i == len(candidates) or target < total:
-                return
-            cur.append(candidates[i])
-            dfs(i, cur, total + candidates[i])
-            cur.pop()
-            dfs(i + 1, cur, total)
+            # add candidates[i]
+            arr.append(candidates[i])
+            dfs(i, arr, curr_sum + candidates[i])
+            # pop candidates[i]
+            arr.pop()
+            dfs(i + 1, arr, curr_sum)
         dfs(0, [], 0)
         return res
         
