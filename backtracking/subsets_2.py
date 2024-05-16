b1e17c795a699d4751fe1e1a2232055612e2dbc8
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2024-05-15 22:28:52 -0400

    Update subsets_2.py

--- backtracking/subsets_2.py
+++ backtracking/subsets_2.py
@@ -2,19 +2,24 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         res = []
+        subset = []
         nums.sort()
-        def backtrack(i, subset):
+    
+        def dfs(i):
             if i == len(nums):
-                res.append(subset[:])
+                res.append(subset.copy())
                 return
-            # all subsets that include nums[i]
+            # add nums[i]
             subset.append(nums[i])
-            backtrack(i + 1, subset)
-            
+            dfs(i + 1)
+            # remove nums[i]
             subset.pop()
-            # all subsets that don't include nums[i]
             while i + 1 < len(nums) and nums[i] == nums[i + 1]:
                 i += 1
-            backtrack(i + 1, subset)
-        backtrack(0, [])
+            dfs(i + 1)
+        dfs(0)
         return res
+'''
+When there are dupes in nums we want to sort and skip over the duplicates
+
+'''
