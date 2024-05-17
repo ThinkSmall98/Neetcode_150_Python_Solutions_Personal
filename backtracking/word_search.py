5cb2c42f56d036658be7124a74473c3ea02ebd77
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2024-05-16 22:39:53 -0400

    Update word_search.py

--- backtracking/word_search.py
+++ backtracking/word_search.py
@@ -4,30 +4,32 @@ from collections import Counter, defaultdict
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         ROWS, COLS = len(board), len(board[0])
-        path = set() # (r, c) 
+        path = set() # (r, c)
 
-        def dfs(r, c, i):
+        def dfs(i, r, c):
             if i == len(word):
                 return True
             if (r >= ROWS or c >= COLS or
-                (r, c) in path or 
+                (r,c) in path or 
                 word[i] != board[r][c] or
                 min(r, c) < 0):
                 return False
-            path.add((r, c))
-            res = (dfs(r + 1, c, i + 1) or 
-                    dfs(r - 1, c, i + 1) or 
-                    dfs(r, c - 1, i + 1) or 
-                    dfs(r, c + 1, i + 1)
-                  )
-            path.remove((r, c)) # backtrack
-            return res 
-        # reverse string if freq of 1st letter greater than last let
+            # add (r, c) in path
+            path.add((r,c))
+            res = (dfs(i + 1, r + 1, c) or 
+                    dfs(i + 1, r - 1, c) or 
+                    dfs(i + 1, r, c + 1) or 
+                    dfs(i + 1, r, c - 1) )
+            # remove (r, c) from path
+            path.remove((r, c))
+
+            return res
+        # reverse string if freq of 1st letter > last letter
         count = Counter(letter for row in board for letter in row)
         if count[0] > count[-1]:
-            count = count[::-1]
+            count[::-1]
         for r in range(ROWS):
             for c in range(COLS):
-                if dfs(r, c, 0):
+                if dfs(0, r, c):
                     return True
         return False
