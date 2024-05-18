c9efbdd3357f287cf25087d70f42101d125f6ada
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2024-05-18 17:29:41 -0400

    Update num_of_islands.py

--- graphs/num_of_islands.py
+++ graphs/num_of_islands.py
@@ -4,25 +4,32 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
-        rows, cols = len(grid), len(grid[0])
-        visited = set()
         islands = 0
+        ROWS, COLS = len(grid), len(grid[0])
+        visited = set() # (r, c)
 
         def bfs(r, c):
             q = deque()
+            # add to queue & to visited
+            q.append((r, c))
             visited.add((r,c))
-            q.append((r,c))
             while q:
                 row, col = q.popleft()
-                directions = ((1, 0), (-1,0), (0,1), (0, -1))
+                directions = ((1,0),(-1,0),(0,1),(0,-1))
                 for dr, dc in directions:
                     r, c = row + dr, col + dc
-                    if r in range(rows) and c in range(cols) and (r,c) not in visited and grid[r][c] == '1':
-                        q.append((r,c))
+                    if (r != ROWS and c != COLS and
+                        min(r, c) >= 0 and
+                        (r,c) not in visited and
+                        grid[r][c] == '1' ):
+                        # add to queue & to visited
+                        q.append((r, c))
                         visited.add((r,c))
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == '1' and (r,c) not in visited:
-                    bfs(r,c)
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == '1' and (r, c) not in visited:
                     islands += 1
+                    bfs(r, c)
         return islands
+
