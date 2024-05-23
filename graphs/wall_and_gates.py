793f122256bcd33c127f959c8396c188fc385f55
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2024-05-22 21:43:20 -0400

    Update wall_and_gates.py

--- graphs/wall_and_gates.py
+++ graphs/wall_and_gates.py
@@ -1,10 +1,13 @@
 # Time: O(m*n), Space: O(m*n)
 from collections import deque
 class Solution:
+    '''
+    BFS bc we want to increment by 1 each time
+    DFS goes too deep
+    '''
     def wallsAndGates(self, rooms: List[List[int]]) -> None:
         ROWS, COLS = len(rooms), len(rooms[0])
         visited = set()
-        
         q = deque()
 
         for r in range(ROWS):
@@ -17,10 +20,17 @@ class Solution:
             for i in range(len(q)):
                 row, col = q.popleft()
                 rooms[row][col] = dist
-                for dr, dc in ((1,0),(-1,0),(0,1),(0,-1)):
+                for dr, dc in ((1,0), (-1,0),(0,1),(0,-1)):
                     r, c = dr + row, dc + col
-                    if (min(r,c) < 0 or r == ROWS or c == COLS or rooms[r][c] == -1 or (r,c) in visited):
+                    if (r == ROWS or c == COLS or 
+                        min(r,c) < 0 or 
+                        (r,c) in visited or
+                        rooms[r][c] == -1):
                         continue
                     q.append((r,c))
                     visited.add((r,c))
             dist += 1
+ '''
+ 1. Initialize queue with gates
+ 2. Do BFS and keep a var. dist to find dist of gates
+ '''
