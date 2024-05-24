37291f4a0c943b90bdc3a2f059d3fb031204a3fc
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2024-05-23 21:25:15 -0400

    Update course_schedule.py

--- graphs/course_schedule.py
+++ graphs/course_schedule.py
@@ -1,10 +1,12 @@
-# Time: O(n + p) where n = numCourses & p = prereqs, Space: O(n + p) 
+# Time: O(n + p) where n = numCourses & p = prereqs
+# Space: O(n + p) 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        preMap = {i:[] for i in range(numCourses)}
+        preMap = {i: [] for i in range(numCourses)}
         for course, prereq in prerequisites:
             preMap[course].append(prereq)
         visited = set()
+
         def dfs(course):
             if course in visited:
                 return False
@@ -12,10 +14,14 @@ class Solution:
                 return True
             visited.add(course)
             for prereq in preMap[course]:
-                if not dfs(prereq): return False
+                if not dfs(prereq):
+                    return False
             visited.remove(course)
-            preMap[course] = [] # set to empty arr so we don't have to repeat work above
+            # set to empty arr bc we already know the prereqs are met
+            preMap[course] = []
             return True
+
         for course in range(numCourses):
-            if not dfs(course): return False
+            if not dfs(course):
+                return False
         return True
