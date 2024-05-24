fe78ff1474fe2ddfc18e730e06f2e342fd165460
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2024-05-23 21:49:15 -0400

    Update course_schedule_2.py

--- graphs/course_schedule_2.py
+++ graphs/course_schedule_2.py
@@ -7,6 +7,11 @@ class Solution:
         output = []
         visited, cycle = set(), set()
 
+        # # Course has 3 possible states:
+        # # 1. visited = course has been added to output
+        # # 2. visiting = course not added to output, but to cycle
+        # # 3. unvisited = course not in output or cycle
+        
         def dfs(course):
             if course in cycle:
                 return False
@@ -20,7 +25,7 @@ class Solution:
             cycle.remove(course)
             output.append(course)
             return True
-
+        
         for c in range(numCourses):
             if dfs(c) == False:
                 return []
