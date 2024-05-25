34b3f50b078e780153e385bdb0af2bf6d6f72456
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2024-05-24 23:18:23 -0400

    Update graph_valid_tree.py

--- graphs/graph_valid_tree.py
+++ graphs/graph_valid_tree.py
@@ -1,21 +1,28 @@
 # Time: O(E + V), Space: O(E + V)
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        if not n:
-            return True
-        adj = {i:[] for i in range(n)}
+        adj = {i: [] for i in range(n)}
         for n1, n2 in edges:
             adj[n1].append(n2)
             adj[n2].append(n1)
         visited = set()
-        def dfs(i, prev):
-            if i in visited:
+        
+        # see if there's a cycle
+        def dfs(curr, prev):
+            if curr in visited:
                 return False
-            visited.add(i)
-            for j in adj[i]:
-                if j == prev:
+            visited.add(curr)
+            for next in adj[curr]:
+                if next == prev:
                     continue
-                if not dfs(j, i):
+                if not dfs(next, curr):
                     return False
             return True
-        return dfs(0, -1) and len(visited) == n
+
+        no_cycle = dfs(0, -1)
+        all_connected_nodes = len(visited) == n
+        return no_cycle and all_connected_nodes
+
+# Check 2 things:
+# 1. Path in b/w every node. num of nodes visited = nodes in tree
+# 2. Make sure no cycle
