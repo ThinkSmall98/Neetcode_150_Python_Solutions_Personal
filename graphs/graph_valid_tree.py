e04c808edc47780bbcd358b9b3d73133af883f3d
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2024-05-24 23:20:55 -0400

    Update graph_valid_tree.py

--- graphs/graph_valid_tree.py
+++ graphs/graph_valid_tree.py
@@ -26,3 +26,9 @@ class Solution:
 # Check 2 things:
 # 1. Path in b/w every node. num of nodes visited = nodes in tree
 # 2. Make sure no cycle
+'''
+Depth-first search is a classic graph-traversal algorithm that can be used to check for both of these conditions:
+
+G is fully connected iff, we started a depth-first search from a single source and discovered all nodes in G during it.
+G contains no cycles iff, the depth-first search never goes back to an already discovered node. 
+We need to be careful though not to count trivial cycles of the form A → B → A that occur with most implementations of undirected edges.'''
