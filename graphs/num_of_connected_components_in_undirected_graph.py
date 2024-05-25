8693816269d26ceba19f9ebff947099fbadf1a27
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2024-05-25 12:24:07 -0400

    Update num_of_connected_components_in_undirected_graph.py

--- graphs/num_of_connected_components_in_undirected_graph.py
+++ graphs/num_of_connected_components_in_undirected_graph.py
@@ -6,26 +6,31 @@
 
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        par = [i for i in range(n)]
+        # initialize each parent of a node to be itself
+        parent = [i for i in range(n)]
         rank = [1] * n
 
-        def find(n):
+        def find_root_parent(n):
             res = n
-            while res != par[res]:
-                par[res] = par[par[res]]
-                res = par[res]
+            while res != parent[res]:
+                # path compression: set parent equal to grandparent
+                parent[res] = parent[parent[res]]
+                res = parent[res]
             return res
 
         def union(n1, n2):
-            p1, p2 = find(n1), find(n2)
+            p1, p2 = find_root_parent(n1), find_root_parent(n2)
+            # Same parent
             if p1 == p2:
                 return 0
+            # p1 is parent of p2
             if rank[p1] > rank[p2]:
-                par[p2] = p1
+                parent[p2] = p1
                 rank[p1] += rank[p2]
+            # p2 is parent of p1
             else:
-                par[p2] = p1
-                rank[p1] += rank[p2]
+                parent[p1] = p2
+                rank[p2] += rank[p1]
             return 1
         res = n
         for n1, n2 in edges:
