2139627ecce15900bc9a77537b4468b3c4d73f49
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2024-05-25 14:49:32 -0400

    Update word_ladder.py

--- graphs/word_ladder.py
+++ graphs/word_ladder.py
@@ -1,4 +1,4 @@
-# Time Complexity: O(M^2*N), where M = len of each word & N = total num of words in the input word list
+# Time Complexity: O(M^2*N), where M = len of each word & N = len(wordList)
 # Space Complexity: O(M^2*N)
 from collections import defaultdict, deque
 class Solution:
@@ -6,12 +6,14 @@ class Solution:
         if endWord not in wordList:
             return 0
         neighbors = defaultdict(list)
-        wordList.append(beginWord)
+        # Generate adjacency list
         for word in wordList:
             for j in range(len(word)):
+                # use '*' as wildcard placeholder
                 pattern = word[:j] + "*" + word[j+1:]
                 neighbors[pattern].append(word)
-        visited = set(beginWord)
+        # shortest path = BFS
+        visited = set()
         q = deque([beginWord])
         res = 1
         while q:
@@ -21,9 +23,10 @@ class Solution:
                     return res
                 for j in range(len(word)):
                     pattern = word[:j] + "*" + word[j+1:]
-                    for neiWord in neighbors[pattern]:
-                        if neiWord not in visited:
-                            visited.add(neiWord)
-                            q.append(neiWord)
+                    for pattern_word in neighbors[pattern]:
+                        if pattern_word not in visited:
+                            visited.add(pattern_word)
+                            q.append(pattern_word)
             res += 1
         return 0
+
