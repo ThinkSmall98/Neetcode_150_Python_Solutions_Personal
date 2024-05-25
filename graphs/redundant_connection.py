00c22514ab4fd66546e8ac2d0394107526f525e5
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2024-05-25 14:03:35 -0400

    Update redundant_connection.py

--- graphs/redundant_connection.py
+++ graphs/redundant_connection.py
@@ -1,18 +1,17 @@
-# Time: O(n) where n = # of vertices. DFS Union has O(alpha(N) Inverse-Ackermann func, approx O(1)
-# Space: O(n). Construction of graph has n nodes
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        # len + 1 bc the nodes start at 1, not 0. Keep 0 empty
         parent = [i for i in range(len(edges) + 1)]
         rank = [1] * (len(edges) + 1)
 
-        def find(n):
-            p = parent[n]
-            while p != parent[p]:
-                parent[p] = parent[parent[p]] # grandparent
-                p = parent[p]
-            return p
+        def findRootParent(n):
+            while n != parent[n]:
+                parent[n] = parent[parent[n]]
+                n = parent[n]
+            return n
+
         def union(n1, n2):
-            p1, p2 = find(n1), find(n2)
+            p1, p2 = findRootParent(n1), findRootParent(n2)
             if p1 == p2: # redundant connection
                 return False
             if rank[p1] > rank[p2]:
