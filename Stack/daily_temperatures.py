9ff998dba9372b74e6109ecdbb82bb16a0f300ea
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2024-06-11 13:26:47 -0400

    Update daily_temperatures.py

--- Stack/daily_temperatures.py
+++ Stack/daily_temperatures.py
@@ -3,10 +3,18 @@ class Solution:
     # Space: O(n)
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         res = [0] * len(temperatures)
-        stack = [] # pair: [temp, index]
+        stack = [] # index, temperature
         for i, temp in enumerate(temperatures):
-            while stack and temp > stack[-1][0]:
-                stack_temp, stack_index = stack.pop()
-                res[stack_index] = i - stack_index
-            stack.append([temp, i])
+            while stack and stack[-1][1] < temp:
+                stack_i, _  = stack.pop()
+                res[stack_i] = i - stack_i
+            stack.append((i, temp))
         return res
+
+'''
+1. Keep stack of tuple (index, temperature)
+2. Go through each temperature in temperatures arr and 
+    if the value of the stack is less than the current temp pop from stack
+    and subtract current day by the day of the stack.
+3. Then outside of while loop append the tuple to stack
+'''
