84abe25d353a713d6cb5afd764f50b4322e83046
ThinkSmall98 <55898378+ThinkSmall98@users.noreply.github.com>
2024-06-12 12:36:36 -0400

    Update meeting_rooms2.py

--- Intervals/meeting_rooms2.py
+++ Intervals/meeting_rooms2.py
@@ -1,5 +1,26 @@
 # Time: O(n * log(n)) where n = len(intervals)
 # Space: O(n)
+from heapq import heappush, heappop
+class Solution:
+    # Approach: heap
+    def minMeetingRooms(self, intervals):
+        if not intervals:
+            return 0
+        q = [] # contains end times of meetings
+        intervals.sort(key = lambda x: x[0])
+        # add 1st meeting end time
+        heappush(q, intervals[0][1])
+        for meeting in intervals[1:]:
+            # if earliest room to be free is available, assign that room to this meeting
+            if q[0] <= meeting[0]:
+                heappop(q)
+            # if new room need to be added
+            heappush(q, meeting[1])
+        return len(q)
+
+
+# Time: O(n * log(n)) where n = len(intervals)
+# Space: O(n)
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         start = sorted(i[0] for i in intervals)
@@ -16,4 +37,3 @@ class Solution:
             res = max(res, count)
         return res
 
-# Can also use a heap
